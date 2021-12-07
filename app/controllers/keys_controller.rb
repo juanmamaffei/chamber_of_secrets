d929d0a5d53e44d17c68b4f6b459daf1247c3688
Juanma <juanmamaffei@gmail.com>
2021-12-07 14:27:00 -0300

    Update passwords

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -29,9 +29,41 @@ module Api
       end
 
       def update
+        key = Key.find(params[:id])
+        action = key.update(
+          title: params[:key][:title],
+          description: params[:key][:description],
+          user: User.find(session[:user_id]),
+          authorized_users: params[:key][:authorized_users],
+          expiration: params[:key][:expiration]
+        )
+        if action
+          render json: {
+            status: 200,
+            key: key
+          }
+        else
+          render json: {
+            status: 500,
+            error: error.details
+          }
+        end
       end
 
       def destroy
+        key = Key.find(params[:id]).destroy
+        if key
+          render json: {
+            status: 200,
+            message: "Your password was deleted."
+          }
+        else
+          render json: {
+            status: 500,
+            error: error.details
+          }
+        end
+        
       end
     end
 

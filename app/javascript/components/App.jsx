4f95e67161714b3c8a52fc1a5c98556a8b9b4f84
Juanma <juanmamaffei@gmail.com>
2021-12-08 02:01:18 -0300

    Login component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Index from './Index'
 import Login from './Session/Login'
+import Dashboard from './Key/Dashboard'
 
-const App = () => (
-    <BrowserRouter>
+const App = () => {
+    return(
+    <BrowserRouter history={history}>
         <Routes>
             <Route path="/" element={ <Index /> } />
             <Route path="/web/login" element={ <Login /> } />
+            <Route path="/web/dashboard" element={ <Dashboard /> } />
+
         </Routes>
     </BrowserRouter>
-)
+)}
 
 export default App
--- app/javascript/components/Session/Login.jsx
+++ app/javascript/components/Session/Login.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, Fragment } from 'react'
-import { Form, Button, Container, Row, Col } from 'react-bootstrap'
+import { Form, Button, Container, Row, Col, Alert } from 'react-bootstrap'
 import axios from 'axios'
+import { useNavigate, Redirect } from 'react-router-dom'
 
-const LoginForm = () => (
-    <Form>
+
+const LoginForm = (props) => (
+    <Form onSubmit={ props.handleSubmit }>
         <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" placeholder="Enter email" name="email" onChange={ props.handleChange } />
             <Form.Text className="text-muted">
             We'll never share your email with anyone else.
             </Form.Text>
@@ -14,7 +16,12 @@ const LoginForm = () => (
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={ props.handleChange }
+            />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Remember me" />
@@ -25,15 +32,57 @@ const LoginForm = () => (
     </Form>
 )
 
-const Login = () => (
-    <Container>
-        <Row>
-            <h2>Login</h2>
-        </Row>
-        <Row>
-            <Col><LoginForm /></Col>
-        </Row>
-    </Container>
-)
+const Messages = (props) => {
+    return(
+        <Alert variant={ props.variant }>{ props.message }</Alert>
+    )
+}
+
+const Login = (props) => {
+    let navigate = useNavigate()
+
+    const [credentials, setCredentials] = useState({ email: "", password: "" })
+    const [alerts, setAlerts] = useState({ variant: "", message: "", show: false })
+    const handleChange = (event) => {
+        setCredentials({ ...credentials, [event.target.name]: event.target.value })
+    }
+    const handleSubmit = (event) => {
+
+        event.preventDefault()
+        const csrf = document.querySelector("[name=csrf-token]").content
+        axios.defaults.headers.common["CSRF-TOKEN"] = csrf
+
+        axios.post("/api/v1/sessions",
+            {
+                "user": {
+                    "email": credentials.email,
+                    "password": credentials.password
+                }
+            }).then((response) => {
+                if (response.data.logged_in) {
+                    navigate("/web/dashboard")
+                    //return(<Redirect to='/web/dashboard' />)
+                } else {
+                    console.log(response)
+                    setAlerts({ variant: "warning", message: response.data.message, show: true })
+
+                }
+            }).catch(response => {
+                setAlerts({ variant: "danger", message: response, show: true })
+            })
+
+    }
+    return (
+        <Container>
+            {alerts.show && <Row><Messages variant={alerts.variant} message={alerts.message} /></Row>}
+            <Row>
+                <h2>Login</h2>
+            </Row>
+            <Row>
+                <Col><LoginForm handleChange={handleChange} handleSubmit={handleSubmit} /></Col>
+            </Row>
+        </Container>
+    )
+}
 
 export default Login

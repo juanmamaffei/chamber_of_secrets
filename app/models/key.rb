463e1524e96778f879219a10912aabd236bc6a66
Juanma <juanmamaffei@gmail.com>
2021-12-17 18:39:54 -0300

    Encryption for keys in database

--- app/models/key.rb
+++ app/models/key.rb
@@ -1,3 +1,5 @@
 class Key < ApplicationRecord
   belongs_to :user
+  encrypts :title, deterministic: true
+  encrypts :description #, deterministic: true, previous: { deterministic: false }
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,16 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    # This is temporary... It should be replaced with environment variables.
-    origins "http://localhost:3000"
-    
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+# Be sure to restart your server when you modify this file.
 
-  end
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
-  allow do
-    # This is temporary... It should be replaced with environment variables.
-    origins "http://localhost:5000"
-    
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+# Read more: https://github.com/cyu/rack-cors
 
-  end
-
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "example.com"
+#
+#     resource "*",
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
